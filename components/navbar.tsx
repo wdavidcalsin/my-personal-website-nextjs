f16b1ce28a636a4c7a8044966ce5d9020d7c9643
Willian David Calsin <david2c142@gmail.com>
2021-09-09 12:33:28 -0500

    reafactor project page (#3)
    * feat: 🎸 added to the list of projects
    
    * refactor: 💡 improved quotation marks, comments and rel
    
    * refactor: 💡 refactor project-page and menu hamburgues
    
    * refactor: 💡 removed unused imports from navbar component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,10 +1,15 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { useState } from 'react';
 import Icon from 'react-icons-kit';
 import { menu } from 'react-icons-kit/feather/menu';
 import WDavidLogo from '../public/WDavid.png';
 
 function Navbar() {
+    const [menuState, setMenuState] = useState(false);
+
+    console.log(menuState);
+
     return (
         <>
             <nav className="w-10/12 lg:w-6/12 flex justify-between items-center pt-5 md:pt-14">
@@ -31,13 +36,43 @@ function Navbar() {
                         </li>
                     </ul>
                 </div>
-                <button className="lg:hidden">
+                <button
+                    className="lg:hidden"
+                    onClick={() => setMenuState(!menuState)}
+                >
                     <Icon
                         icon={menu}
                         size=""
                         className="w-8 md:w-12 text-primary "
                     />
                 </button>
+                <div
+                    className={`w-9/12 lg:hidden h-full bg-secondary absolute flex flex-col items-center top-0 z-10 transition-all duration-300 ${
+                        menuState ? 'left-0' : '-left-full'
+                    }`}
+                >
+                    <div className="mt-8 space-y-10">
+                        <h2 className="text-quartet font-comfortaa text-3xl text-center font-bold">
+                            Menu
+                        </h2>
+                        <ul className=" text-quartet flex flex-col items-center font-comfortaa text-2xl space-y-10">
+                            <li>
+                                <Link href="/about">
+                                    <a className="hover:opacity-80 transition duration-300">
+                                        About
+                                    </a>
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/projects">
+                                    <a className="hover:opacity-80  transition duration-300">
+                                        Projects
+                                    </a>
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </nav>
         </>
     );
